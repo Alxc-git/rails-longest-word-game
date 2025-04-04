0c1c104ea4bf53b153ea2e914d68d19ec8205b83
Alexis Giard <alexis.giard96@gmail.com>
2025-04-03 20:36:08 -0400

    rails-longest-word-game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,13 +5,22 @@ class GamesController < ApplicationController
 
   def new
     @liste = Array.new(10) { rand(97..122).chr }
+    session[:score] = 0
   end
 
   def score
-    @word = params[:word]
+    @word = params[:word].upcase
     url = URI("https://dictionary.lewagon.com/#{@word}")
     response = Net::HTTP.get(url)
     @data = JSON.parse(response)
-    raise # Trouver comment recuperer le @data[:found] si il est true ou false
+
+    if @data['found']
+      session[:score] += @word.length
+    else
+      session[:score] = 0
+    end
+
+    @score = session[:score]
+    raise
   end
 end
